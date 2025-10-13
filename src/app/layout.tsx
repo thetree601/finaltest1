e3645b152ac5694cc071e946ec4266f9db2fbd61
thetree601 <mytalk481@gmail.com>
2025-10-13 20:31:22 +0900

    feat: next-themes 프로바이더 구현 및 다크모드 지원 추가
    - NextThemesProvider 컴포넌트 구현 (시스템 테마 감지 지원)
    - layout.tsx에서 Provider 연결 및 children 래핑 구조 적용
    - globals.css에 클래스 기반 다크모드 CSS 변수 추가
    - 시스템 기본 테마 및 수동 테마 전환 모두 지원

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { ModalProvider } from "@/commons/providers/modal/modal.provider";
+import { NextThemesProvider } from "@/commons/providers/next-themes/next-themes.provider";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -29,9 +30,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ModalProvider>
-          {children}
-        </ModalProvider>
+        <NextThemesProvider>
+          <ModalProvider>
+            {children}
+          </ModalProvider>
+        </NextThemesProvider>
       </body>
     </html>
   );
